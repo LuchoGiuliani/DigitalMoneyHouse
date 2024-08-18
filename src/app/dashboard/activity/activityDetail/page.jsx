6db955748f7ce22b48385a9b69958b9f803ac1ba
unknown <luciano.giuliani92@gmail.com>
2024-08-18 13:06:01 -0300

    Agregamos carpetas para terminar el sprint3, cargar dinero, activity, etc.

---
+++ src/app/dashboard/activity/activityDetail/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Activity Detail</div>
+  )
+}
+
+export default page
--- src/app/dashboard/activity/page.jsx
+++ src/app/dashboard/activity/page.jsx
@@ -1,14 +1,29 @@
 import LeftSidebar from '@/components/LeftSidebar/LeftSidebar'
+import Image from 'next/image'
 
 import React from 'react'
 
 const page = () => {
   return (
-    <main className="bg-gray-200">
+    <main className="bg-color-gray">
       <section className="flex">
         <LeftSidebar />
-        <h1>Activity</h1>
-         
+        <div className='p-10 w-full flex  flex-col gap-4'>
+          <div className='flex w-full gap-4'>
+          <div className='bg-white w-full p-2 flex gap-2 rounded-lg drop-shadow-lg'>
+            <Image src={"/search.png"} width={12} height={0} className='w-auto h-auto' alt='search'   />
+            <h1 className='text-gray-400'>Buscar en tu actividad</h1>
+          </div>
+          <button className='bg-color-primary p-2 rounded-lg drop-shadow-lg min-w-[164px] flex gap-2 items-center font-semibold'>
+             Filtrar
+             <Image  src={"/filter.png"} width={12} height={0} className='w-auto h-auto' alt='search'  />
+          </button>
+          </div>
+          <div className='w-full h-full bg-white rounded-lg drop-shadow-lg p-2'>
+            Activity
+          </div>
+
+        </div>
       </section>
     </main>
   )
