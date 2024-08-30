7adf848b7bf2405101cc8d3a2a69c5e888e517fe
unknown <luciano.giuliani92@gmail.com>
2024-08-30 19:11:23 -0300

    Cambios en loginForm

--- src/app/(user)/login/page.jsx
+++ src/app/(user)/login/page.jsx
@@ -3,7 +3,7 @@ import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import LoginForm from "@/components/LoginForm/LoginForm";
 
-const Page = () => {
+const page = () => {
   const searchParams = useSearchParams();
   const email = searchParams.get("email");
 
@@ -25,4 +25,4 @@ const Page = () => {
   );
 };
 
-export default Page;
+export default page;
