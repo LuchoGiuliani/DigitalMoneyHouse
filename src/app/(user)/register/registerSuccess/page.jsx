efec241e30e973e0af31f3cab08590a62873f89d
unknown <luciano.giuliani92@gmail.com>
2024-08-31 20:55:22 -0300

    Cambiamos el registerSuccess

--- src/app/(user)/register/registerSuccess/page.jsx
+++ src/app/(user)/register/registerSuccess/page.jsx
@@ -6,14 +6,17 @@ import Link from 'next/link'
 
 const RegisterSuccess = () => {
   return (
-    <div className="h-screen bg-color-darker flex flex-col justify-between">
-      <div className="flex flex-col items-center justify-center flex-grow">
+    <div className="h-screen bg-color-darker flex flex-col justify-between ">
+      <div className="flex flex-col items-center justify-center flex-grow  gap-4">
+        <h1 className='text-white font-bold text-4xl'>Registro exitoso</h1>
         <Image 
-          src="/RegistroExitoso.png" 
+          src="/checkGreen.svg" 
           alt="Registro Exitoso" 
-          width={500} 
-          height={500} 
+          width={12} 
+          height={12} 
+          className='h-auto w-auto'
         />
+        <p className='text-white text-[18px] text-center max-w-[450px]'>Hemos enviado un correo de confirmación para validar tu email, por favor revisalo para iniciar sesión.</p>
         <Link className="w-fit p-2 mt-4 rounded-md bg-color-primary" href="/login" passHref>
           Continuar
         </Link>
