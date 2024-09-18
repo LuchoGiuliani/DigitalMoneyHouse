0eea74857414e25b2d067b18bb221d78c02fc08f
unknown <luciano.giuliani92@gmail.com>
2024-09-18 19:15:29 -0300

    Cambios generales: Agregamos listado de servicios

--- src/app/dashboard/payServices/page.jsx
+++ src/app/dashboard/payServices/page.jsx
@@ -1,21 +1,19 @@
-"use client"
+"use client";
 import getServices from "@/services/getServices";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 const page = () => {
- const [services, setServices] = useState([])
+  const [services, setServices] = useState([]);
 
- useEffect(()=> {
-  getServices(setServices)
-
- },[])
-console.log(services);
-
+  useEffect(() => {
+    getServices(setServices);
+  }, []);
+  console.log(services);
 
   return (
     <div className="bg-color-gray min-h-screen p-8 flex flex-col gap-4">
-            <div className="flex gap-2 tablet:hidden">
+      <div className="flex gap-2 tablet:hidden">
         <Image
           src="/arrowGray.svg"
           width={12}
@@ -30,50 +28,18 @@ console.log(services);
       </div>
       <div className="rounded-lg bg-white flex flex-col gap-4 p-6">
         <h2 className="font-bold pb-4 border-b ">Mas recientes</h2>
+
         <div className="flex gap-2 justify-between border-b pb-2">
-          <div className="flex gap-2">
-          <Image
-                src={"/logoClaro.svg"}
-                width={12}
-                height={0}
-                className="w-auto h-auto"
-                alt="search"
-              />
-            <div>Claro</div>
+          <div className="flex flex-col gap-2 w-full">
+            {services &&
+              services.map((sv) => (
+                <div className="flex justify-between border-b py-2">
+                  <h1>{sv.name}</h1>
+                  <button className="font-bold">Seleccionar</button>
+                </div>
+              ))}
           </div>
-          <div>Seleccionar</div>
         </div>
-        <div className="flex gap-2 justify-between border-b pb-2">
-          <div className="flex gap-2">
-          <Image
-                src={"/logoCablevision.svg"}
-                width={12}
-                height={0}
-                className="w-auto h-auto"
-                alt="search"
-              />
-            <div>Cablevisión</div>
-          </div>
-          <div>Seleccionar</div>
-        </div>
-        <div className="flex gap-2 justify-between border-b pb-2">
-          <div className="flex gap-2">
-          <Image
-                src={"/logoPersonal.svg"}
-                width={12}
-                height={0}
-                className="w-auto h-auto"
-                alt="search"
-              />
-            <div>Personal</div>
-          </div>
-          <div>Seleccionar</div>
-        </div>
-      </div>
-      <div>
-        {services && services.map((sv) => (
-          <div>{sv.name}</div>
-        ))}
       </div>
     </div>
   );
